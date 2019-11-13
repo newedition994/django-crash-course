52ba4e94f214c14c32972ebc46473a69852725a5
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 16:19:37 -0500

    gathering detail layout

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -7,4 +7,6 @@ from .models import Question, Choice
 
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
