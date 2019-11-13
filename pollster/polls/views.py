24020d8c362de138aaf5477add3d047204c6e72e
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 16:38:41 -0500

    completed the views

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -29,3 +29,20 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+# Vote on a question
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect('polls:results', args=(question_id,))
