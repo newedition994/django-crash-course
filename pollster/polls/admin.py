e5b556c50960a75ce80b961eaeb435c13df23b1a
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 15:26:48 -0500

    changed the header

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
