c6de0f4b0644f953340f1ca4d2c4a77caf7f63b5
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 16:33:10 -0500

    getting the details and results to display

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
 
 # Create your views here.
 from .models import Question, Choice
@@ -10,3 +14,18 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# Display questions and choices
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
