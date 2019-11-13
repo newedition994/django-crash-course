9ca14f2f88d006c2acebcbcd2f9bf05bf7029f37
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 15:32:17 -0500

    connecting paths to polls url

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Question, Choice
+
+# Get Questions for display
+
+
+def index(request):
+    return render(request, 'polls/index.html')
