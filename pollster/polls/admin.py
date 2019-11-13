ef7e47326623e87bc4344a080e3012de03987c62
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 15:24:07 -0500

    revised the way the question and choices are viewed

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {
+        'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
