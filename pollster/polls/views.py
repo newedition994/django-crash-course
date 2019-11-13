1529a53f3c7251450d7ace937dd2ef796f94651b
Breyanna Vass <breyannavass@gmail.com>
2019-11-13 16:43:43 -0500

    reversed back to results

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -45,4 +45,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect('polls:results', args=(question_id,))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
